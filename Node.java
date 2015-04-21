b7a76ce9d89f4123af2b3e58ae225f13287f2276
p512 <point6000x+gh@gmail.com>
2015-04-22 01:26:29 +0200

    Method declarations

--- Node.java
+++ Node.java
@@ -1,3 +1,5 @@
 public class Node {
-	
+	private Node _left, _right;
+	private int _key;
+	private int _size;
 }
--- Tree.java
+++ Tree.java
@@ -1,3 +1,43 @@
 public class Tree {
+	private Node _root;
 	
+	public boolean isEmpty() {
+		return true;
+	}
+	
+	public int size() {
+		return 0;
+	}
+	
+	public boolean exists(int val) {
+		return true;
+	}
+	
+	public int height() {
+		return 0;
+	}
+	
+	public void insert(int val) {
+		
+	}
+	
+	public void delete(int val) {
+		
+	}
+	
+	public int valueAtPosition(int k) {
+		return 0;
+	}
+	
+	public int position(int val) {
+		return 0;
+	}
+	
+	Iterable<Integer> values(int lo, int hi) {
+		return null;
+	}
+	
+	public void simpleBalance() {
+		
+	}
 }
